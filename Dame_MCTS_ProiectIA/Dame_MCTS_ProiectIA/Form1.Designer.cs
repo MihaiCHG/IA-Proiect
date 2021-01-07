dc0e1f5a072611257c31e94f9f454868cefc2e7b
MihaiCHG <52129403+MihaiCHG@users.noreply.github.com>
2021-01-07 14:29:24 +0200

    Add files via upload

---
+++ Dame_MCTS_ProiectIA/Dame_MCTS_ProiectIA/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Dame_MCTS_ProiectIA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.labelOutputAction = new System.Windows.Forms.Label();
+            this.labelPlayerTurn = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 560);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Location = new System.Drawing.Point(627, 162);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonNewGame.TabIndex = 1;
+            this.buttonNewGame.Text = "New Game";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            this.buttonNewGame.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelOutputAction
+            // 
+            this.labelOutputAction.AutoSize = true;
+            this.labelOutputAction.Location = new System.Drawing.Point(594, 87);
+            this.labelOutputAction.Name = "labelOutputAction";
+            this.labelOutputAction.Size = new System.Drawing.Size(97, 13);
+            this.labelOutputAction.TabIndex = 2;
+            this.labelOutputAction.Text = "Selecteaza o piesa";
+            // 
+            // labelPlayerTurn
+            // 
+            this.labelPlayerTurn.AutoSize = true;
+            this.labelPlayerTurn.Location = new System.Drawing.Point(594, 117);
+            this.labelPlayerTurn.Name = "labelPlayerTurn";
+            this.labelPlayerTurn.Size = new System.Drawing.Size(121, 13);
+            this.labelPlayerTurn.TabIndex = 3;
+            this.labelPlayerTurn.Text = "Randul jucatorului uman";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 596);
+            this.Controls.Add(this.labelPlayerTurn);
+            this.Controls.Add(this.labelOutputAction);
+            this.Controls.Add(this.buttonNewGame);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.Label labelOutputAction;
+        private System.Windows.Forms.Label labelPlayerTurn;
+    }
+}
+
---
+++ Dame_MCTS_ProiectIA/Dame_MCTS_ProiectIA/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dame_MCTS_ProiectIA
+{
+    public partial class Form1 : Form
+    {
+        private CellType[,] board;
+        private ActionType action;
+        private bool humanTurn;
+        private Point currentPos, newPos;
+        private enum CellType { White, Black, BlackWithX, BlackWithY };
+        private enum ActionType { ToSelect, ToMove };
+        public Form1()
+        {
+            InitializeComponent();
+            board = new CellType[8, 8] {
+                { CellType.White, CellType.BlackWithY, CellType.White, CellType.BlackWithY, CellType.White, CellType.BlackWithY, CellType.White, CellType.BlackWithY},
+                { CellType.BlackWithY, CellType.White, CellType.BlackWithY, CellType.White, CellType.BlackWithY, CellType.White, CellType.BlackWithY, CellType.White},
+                { CellType.White, CellType.BlackWithY, CellType.White, CellType.BlackWithY, CellType.White, CellType.BlackWithY, CellType.White, CellType.BlackWithY},
+                { CellType.Black, CellType.White, CellType.Black, CellType.White, CellType.Black, CellType.White, CellType.Black, CellType.White},
+                { CellType.White, CellType.Black, CellType.White, CellType.Black, CellType.White, CellType.Black, CellType.White, CellType.Black},
+                { CellType.BlackWithX, CellType.White, CellType.BlackWithX, CellType.White, CellType.BlackWithX, CellType.White, CellType.BlackWithX, CellType.White},
+                { CellType.White, CellType.BlackWithX, CellType.White, CellType.BlackWithX, CellType.White, CellType.BlackWithX, CellType.White, CellType.BlackWithX},
+                { CellType.BlackWithX, CellType.White, CellType.BlackWithX, CellType.White, CellType.BlackWithX, CellType.White, CellType.BlackWithX, CellType.White},
+            };
+            this.action = ActionType.ToSelect;
+            this.humanTurn = true;
+        }
+
+        private void drawBoard()
+        {
+            pictureBox1.Image = new Bitmap("images\\board.png");
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        switch (board[j, i])
+                        {
+                            case CellType.Black:
+                                g.DrawImage(new Bitmap("images\\blackBox.png"), new Point(i * 70, j * 70));
+                                break;
+                            case CellType.BlackWithX:
+                                g.DrawImage(new Bitmap("images\\blackBox.png"), new Point(i * 70, j * 70));
+                                g.DrawImage(new Bitmap("images\\pieceX.png"), new Point(i * 70, j * 70));
+                                break;
+                            case CellType.BlackWithY:
+                                g.DrawImage(new Bitmap("images\\blackBox.png"), new Point(i * 70, j * 70));
+                                g.DrawImage(new Bitmap("images\\pieceY.png"), new Point(i * 70, j * 70));
+                                break;
+                        }
+                    }
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            drawBoard();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<Point> getAvailableMovesForPiece(int line, int column)
+        {
+            List<Point> moves = new List<Point>();
+            return moves;
+        }
+
+        private void makeMove(Point currentPos, Point newPos)
+        {
+            if (board[currentPos.X, currentPos.Y] != CellType.White && board[newPos.X, newPos.Y] != CellType.White)
+            {
+                CellType aux;
+                aux = board[currentPos.X, currentPos.Y];
+                board[currentPos.X, currentPos.Y] = board[newPos.X, newPos.Y];
+                board[newPos.X, newPos.Y] = aux;
+            }
+        }
+
+        private bool isValidMove(Point currentPos, Point newPos)
+        {
+            if (humanTurn && board[currentPos.X, currentPos.Y] != CellType.BlackWithX)
+                return false;
+            if (!humanTurn && board[currentPos.X, currentPos.Y] != CellType.BlackWithY)
+                return false;
+            if (board[newPos.X, newPos.Y] != CellType.Black)
+                return false;
+            return true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            int line = e.Location.Y / 70;
+            int column = e.Location.X / 70;
+            if (action == ActionType.ToSelect)
+            {
+                currentPos.X = line;
+                currentPos.Y = column;
+                if (humanTurn && board[currentPos.X, currentPos.Y] != CellType.BlackWithX)
+                    action = ActionType.ToSelect;
+                else if (!humanTurn && board[currentPos.X, currentPos.Y] != CellType.BlackWithY)
+                    action = ActionType.ToSelect;
+                else
+                    action = ActionType.ToMove;
+            }
+            else if (action == ActionType.ToMove)
+            {
+                newPos.X = line;
+                newPos.Y = column;
+                if (isValidMove(currentPos, newPos))
+                {
+                    makeMove(currentPos, newPos);
+                    drawBoard();
+                    humanTurn = !humanTurn;
+                    action = ActionType.ToSelect;
+                }
+                else
+                    action = ActionType.ToMove;
+            }
+            if (action == ActionType.ToMove)
+                labelOutputAction.Text = "Muta piesa";
+            else
+                labelOutputAction.Text = "Selecteaza o piesa";
+            if (humanTurn)
+                labelPlayerTurn.Text = "Randul jucatorului uman";
+            else
+                labelPlayerTurn.Text = "Randul calculatorului";
+        }
+    }
+}
+
